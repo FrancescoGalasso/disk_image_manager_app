8e5d960cba3eb0059ca5c719b9fc19d3aab0c6cc
FrancescoGalasso <francescogalasso@alfadispenser.com>
2021-01-15 11:24:53 +0100

    tidy up tests

--- tests/test_drivelist.py
+++ tests/test_drivelist.py
@@ -1,9 +1,10 @@
+import os
 import pytest
 from context import drivelist
 
 
 def test_get_drive_list():
-    test_path = './drivelist.txt'
+    test_path = os.ah.abspath(os.path.join(os.path.dirname(__file__), 'drivelist.txt'))
     lista = drivelist.get_drive_list(by_cmd_lsblk=False, from_file_path=test_path)
 
     assert len(lista) != 0
@@ -13,10 +14,10 @@ def test_get_drive_list():
         assert isinstance(el, drivelist.Drive) == True
 
     assert lista[0].device == '/dev/sda'
-    assert lista[0].check_writeability()
+    assert lista[0].check_writeability() == True
 
     assert lista[1].device == '/dev/sdb'
-    assert lista[1].check_writeability()
+    assert lista[1].check_writeability() == True
 
     assert lista[2].device == '/dev/mmcblk0'
     assert lista[2].check_writeability() == False
