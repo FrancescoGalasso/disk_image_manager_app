66817a59186729dc61dc50169aafd11e11840fe1
FrancescoGalasso <francescogalasso@alfadispenser.com>
2021-01-21 17:19:10 +0100

    v0.1.0

--- dima/__version__.py
+++ dima/__version__.py
@@ -1,7 +1,7 @@
 __title__ = 'dima'
 __description__ = 'disk image manager app'
-__version__ = '0.0.1'
-__build__ = 0x000001
+__version__ = '0.1.0'
+__build__ = 0x000100
 __author__ = 'Francesco Galasso'
 __author_email__ = 'francescogalasso@alfadispenser.com'
 __license__ = 'MIT'
